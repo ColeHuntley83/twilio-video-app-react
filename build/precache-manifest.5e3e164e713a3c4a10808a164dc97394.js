2ce2fd93dd1bc1634ce143854997573c12a0b1b3
ColeHuntley83 <colehuntley@MacBook-Pro.local>
2020-06-09 15:05:06 -0700

    ubuntu server

---
+++ build/precache-manifest.5e3e164e713a3c4a10808a164dc97394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c905a312d49983dfd9e75efc78ef06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a90b0e63441a50e630",
+    "url": "/static/js/2.9a2d1f3a.chunk.js"
+  },
+  {
+    "revision": "8dec489cc9fea776161524e157527d3e",
+    "url": "/static/js/2.9a2d1f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12e4048a55028518f82",
+    "url": "/static/js/main.dbca8146.chunk.js"
+  },
+  {
+    "revision": "7a3fe74a1db7f2f594a1",
+    "url": "/static/js/runtime-main.fa60846a.js"
+  },
+  {
+    "revision": "1b8c51ce350862c46a8e50112049fd52",
+    "url": "/static/media/google-logo.1b8c51ce.svg"
+  },
+  {
+    "revision": "0e04ea745e2c6f1fec53ca74a52de526",
+    "url": "/static/media/twilio-logo.0e04ea74.svg"
+  }
+]);
--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,7 @@ You can control this with the homepage field in your package.json.
 The build folder is ready to be deployed.
 You may serve it with a static server:
 
-  npm install -g serve
+  
   serve -s build
 
 Find out more about deployment here:
